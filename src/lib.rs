1790c7c63d178ef0cabb5721bf8e5577154611be
Malena Maffei <malenamaffei@192.168.1.5>
2022-04-11 14:58:35 -0300

    refactors, removed warnings, fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,3 @@
-use std::error::Error;
-mod file;
-mod grid;
-
-pub fn run(filename_a: String, filename_b: String) -> Result<(), Box<dyn Error>> {
-    let mut file_a = file::File::new(filename_a)?;
-    let mut file_b = file::File::new(filename_b)?;
-
-    let mut lcs_grid = grid::LCSGrid::new(file_a.len_lines, file_b.len_lines);
-
-    lcs_grid.construct_grid(&mut file_a.contents, &mut file_b.contents);
-    print_diff(
-        &lcs_grid.grid,
-        &file_a.contents,
-        &file_b.contents,
-        file_a.contents.len(),
-        file_b.contents.len(),
-    );
-    Ok(())
-}
-
 pub fn print_diff(
     grid: &Vec<Vec<usize>>,
     lines_a: &Vec<String>,
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::process;
 mod file;
 mod filenamesparser;
+use std::error::Error;
+mod grid;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,8 +16,25 @@ fn main() {
     println!("Generating diff for {}", file_names.file_a);
     println!("Against {}", file_names.file_b);
 
-    if let Err(e) = taller_diff::run(file_names.file_a, file_names.file_b) {
+    if let Err(e) = run(file_names.file_a, file_names.file_b) {
         println!("Application error: {}", e);
         process::exit(1);
     }
+}
+
+pub fn run(filename_a: String, filename_b: String) -> Result<(), Box<dyn Error>> {
+    let mut file_a = file::File::new(filename_a)?;
+    let mut file_b = file::File::new(filename_b)?;
+
+    let mut lcs_grid = grid::LCSGrid::new(file_a.len_lines, file_b.len_lines);
+
+    lcs_grid.construct_grid(&mut file_a.contents, &mut file_b.contents);
+    taller_diff::print_diff(
+        &lcs_grid.grid,
+        &file_a.contents,
+        &file_b.contents,
+        file_a.contents.len(),
+        file_b.contents.len(),
+    );
+    Ok(())
 }
